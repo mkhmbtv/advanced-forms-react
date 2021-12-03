6aa28ced47162766396b870dcbf5a900b7556c5c
your name here <mmakhambetov@yandex.ru>
2021-12-03 14:01:54 +0400

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from './components/Home';
-import logo from './logo.png';
 
 function App() {
   return (
     <div className='page-wrapper'>
-      <header className='header'>
-        <img className='header__logo' src={logo} alt='logo' />
-        <h1>Survey Tool</h1>
-      </header>
       <Navigation />
       <Switch>
-        <Route path='/'>
+        <Route exact path='/'>
           <Home />
         </Route>
       </Switch>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import './Navigation.css';
+import logo from '../../logo.png';
 
 const Navigation = () => {
   return (
-    <nav>
-      <NavLink to='/sample'>Sample Survey</NavLink>
-      <NavLink to='/sp'>Sensory Preferences</NavLink>
-      <NavLink to='/report'>Report</NavLink>
-      <NavLink exact to='/'>Home</NavLink>
-    </nav>
+    <>
+      <header className='header'>
+        <img className='header__logo' src={logo} alt='logo' />
+        <h1>Survey Tool</h1>
+      </header>
+      <nav>
+        <NavLink to='/sample'>Sample Survey</NavLink>
+        <NavLink to='/spi'>Sensory Preferences</NavLink>
+        <NavLink to='/report'>Report</NavLink>
+        <NavLink exact to='/'>Home</NavLink>
+      </nav>
+    </>
   )
 };
 
